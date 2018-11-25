a949584a479b5495ba08cd5e4b64556e44362a2b
architg0409 <43095929+architg0409@users.noreply.github.com>
2018-11-25 12:42:48 +0530

    Updated manual.py
    Have Changed button colors and labels.

--- Project/manual.py
+++ Project/manual.py
@@ -19,11 +19,11 @@ Login1Label=Label(window,text="Classroom Automation",font=("Courier",43),fg="gre
 
 Login2Label=Label(window,text="Welcome to Manual System",font=("Courier",35),fg="green yellow",bg="lemon chiffon")
 
-LoginLabel=Label(window,text="Choose to turn-on OR turn-off:",font=("Courier",28),fg="green yellow",bg="lemon chiffon")
+LoginLabel=Label(window,text="Choose to Turn-on OR Turn-off:",font=("Courier",28),fg="green yellow",bg="lemon chiffon")
 
-SubmitButton=Button(window,text="Turn-ON",fg="peachpuff4",font=f,bg="lemon chiffon",command=On)
+TurnOnButton=Button(window,text="Turn-ON",fg="white",font=f,bg="green",command=On)
 
-Submit2Button=Button(window,text="Turn-OFF",fg="peachpuff4",font=f,bg="lemon chiffon",command=Off)
+TurnOffButton=Button(window,text="Turn-OFF",fg="white",font=f,bg="green",command=Off)
 
 Login1Label.place(x=123,y=30)
 
@@ -31,9 +31,9 @@ Login2Label.place(x=130,y=120)
 
 LoginLabel.place(x=140,y=250)
 
-SubmitButton.place(x=270,y=350)
+TurnOnButton.place(x=270,y=350)
 
-Submit2Button.place(x=540,y=350)
+TurnOffButton.place(x=540,y=350)
 
 
 window.mainloop()
