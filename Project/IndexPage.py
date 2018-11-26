1351ef9b07321a699a534b308d18aa47a544872b
architg0409 <43095929+architg0409@users.noreply.github.com>
2018-11-26 18:19:47 +0530

    Updated Font Style and Label Colors

--- Project/IndexPage.py
+++ Project/IndexPage.py
@@ -13,29 +13,29 @@ window.title("Login")
 window.geometry('900x700')
 window.configure(background="lemon chiffon")
 f=("Artifika",14)
-f1=("Courier",24)
+f1=("Artifika",24)
 
 Login1Label=Label(window,text="Classroom Automation",font=("Artifika",40),fg="steel blue",bg="lemon chiffon")
 
 LoginLabel=Label(window,text="Choose your option:",font=("Artifika",30),fg="steel blue",bg="lemon chiffon")
 
 AutoLabel=Label(window,text="(It will put your system in Automatic Mode)",font=f,fg="medium sea green",bg="lemon chiffon")
-AutoButton=Button(window,text="Automatic",fg="white",font=f1,bg="green yellow",command=display1)
+AutoButton=Button(window,text="Automatic",fg="white",font=f1,bg="green",command=display1)
 
 
 ManLabel=Label(window,text="(It will put your system in Manual Mode)",font=f,fg="medium sea green",bg="lemon chiffon")
-ManButton=Button(window,text="Manual",fg="white",font=f1,bg="green yellow",command=display2)
+ManButton=Button(window,text="Manual",fg="white",font=f1,bg="green",command=display2)
 
-Login1Label.place(x=140,y=50)
+Login1Label.place(x=160,y=50)
 
-LoginLabel.place(x=230,y=150)
+LoginLabel.place(x=260,y=150)
 
 
 
 AutoButton.place(x=350,y=280)
-AutoLabel.place(x=230,y=360)
+AutoLabel.place(x=265,y=360)
 ManButton.place(x=350,y=470)
-ManLabel.place(x=245,y=550)
+ManLabel.place(x=280,y=550)
 AutoButton.config( height = 1, width = 10 )
 ManButton.config( height = 1, width = 10 )
 window.mainloop()
